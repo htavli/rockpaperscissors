6cbd0d98d23d61e3698122c82a3a6b82852dc093
htavli <htavli@proton.me>
2024-07-12 23:53:47 +0300

    Finished the full game

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 let options = ["rock", "paper", "scissors"];
 
+function clear_input(input) {
+  let res = "";
+  for (let i = 0; i < input.length; i++) {
+    res += input[i].toLowerCase();
+  }
+  return res;
+}
+
 function getComputerChoice() {
   let choice = Math.random() * 3;
   console.log(choice);
@@ -10,7 +18,9 @@ function getComputerChoice() {
 function getHumanChoice() {
   let choice = "";
   while (true) {
-    choice = prompt("Which one will you play : rock, paper, or scissors?");
+    choice = clear_input(
+      prompt("Which one will you play : rock, paper, or scissors?")
+    );
     if (options.includes(choice)) {
       switch (choice) {
         case "rock":
@@ -28,34 +38,77 @@ function getHumanChoice() {
   }
 }
 
-let computerChoice = getComputerChoice();
-let humanChoice = getHumanChoice();
-
-switch (humanChoice - computerChoice) {
-  case 0:
-    alert("It's a draw!");
-    break;
-  case 1:
-  case -2:
-    endGame("you", "the computer");
-    break;
-  case -1:
-  case 2:
-    endGame("the computer", "you");
-    break;
+function decideWinner(humanChoice, computerChoice) {
+  switch (humanChoice - computerChoice) {
+    case 0:
+      return "draw";
+    case 1:
+    case -2:
+      return "player";
+    case -1:
+    case 2:
+      return "computer";
+  }
 }
 
 function endGame(winner, loser) {
   console.log(winner + " beat " + loser + " !");
+  if ((winner = "you")) {
+    humanScore++;
+  } else {
+    computerScore++;
+  }
 }
 
-console.log(
-  "Your choice: " +
-    options[humanChoice] +
-    "\nComputer's choice: " +
-    options[computerChoice]
-);
+function playGame(play_for_x_times) {
+  let humanScore = 0;
+  let computerScore = 0;
+
+  let computerChoice;
+  let humanChoice;
+
+  let winner;
+
+  for (let i = 0; i < play_for_x_times; i++) {
+    computerChoice = getComputerChoice();
+    humanChoice = getHumanChoice();
+
+    winner = decideWinner(humanChoice, computerChoice);
+
+    switch (winner) {
+      case "draw":
+        console.log(
+          `It's a draw! Your choice: ${options[humanChoice]} Computer's choice: ${options[computerChoice]}`
+        );
+        break;
+      case "player":
+        console.log(
+          `Player wins! Your choice: ${options[humanChoice]} Computer's choice: ${options[computerChoice]}`
+        );
+        humanScore++;
+        break;
+      case "computer":
+        console.log(
+          `The computer wins! Your choice: ${options[humanChoice]} Computer's choice: ${options[computerChoice]}`
+        );
+        computerScore++;
+        break;
+    }
+  }
+
+  console.log(`The score is: Player ${humanScore} - ${computerScore} Computer`);
+  if (humanScore > computerScore) {
+    console.log("The player wins!");
+  } else if (humanScore < computerScore) {
+    console.log("The computer wins!");
+  } else {
+    console.log("It's a draw!");
+  }
+}
 
 function Main() {
   console.log("Welcome to the game!");
+  playGame(2);
 }
+
+Main();
